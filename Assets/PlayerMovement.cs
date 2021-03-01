534ec07f2298661f6d5bf74ca8eced5dc5f71f61
Robert <robert@pepzi.org>
2021-03-01 18:31:38 +0100

    Refactor PlayerMovement, adds flip x when moving left.

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -1,25 +1,54 @@
+using System;
 using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
     public float speed = 4.0f;
+
     private Rigidbody2D rigidBody2D;
+    private Vector2 _velocity;
 
     private void Awake()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
+        _velocity = Vector2.zero;
     }
 
     private void Update()
     {
-        float movex = Input.GetAxisRaw("Horizontal");
-        float movey = Input.GetAxisRaw("Vertical");
+        HandleInput();
+        UpdateAnimator();
+        MovePlayer();
+    }
 
-        Vector3 move = new Vector2(movex, movey);
-        move.Normalize();
+    private void UpdateAnimator()
+    {
+        if (_velocity.x > 0)
+        {
+            transform.localScale = new Vector2(1.0f, 1.0f);
+        }
+        if (_velocity.x < 0)
+        {
+            transform.localScale = new Vector2(-1.0f, 1.0f);
+        }
+        if (_velocity.y > 0)
+        {
 
+        }
+        if (_velocity.y < 0)
+        {
 
-        rigidBody2D.velocity = move* speed;
-//        rigidBody2D.transform.position += move * (speed * Time.deltaTime);
+        }
+    }
+
+    private void MovePlayer()
+    {
+        rigidBody2D.velocity = _velocity * speed;
+    }
+
+    private void HandleInput()
+    {
+        _velocity = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
+        _velocity.Normalize();
     }
 }
